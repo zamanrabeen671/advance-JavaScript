a08bfa57e3dc5c8a395495d657381c7539811fd7
zamanrabeen671 <manbeen671@gmail.com>
2021-02-02 00:18:17 +0600

    null vs undefined

---
+++ nullvsundefined.js
@@ -0,0 +1,11 @@
+let pakhi;
+function add(s,t) {
+    console.log(s+t);
+}
+function value(n,m) {
+    console.log(n,m);
+}
+const premik = {Name: "romeo", phone: 43434}
+let result = add(10,5);
+let result2 = value(10);
+console.log(pakhi);
