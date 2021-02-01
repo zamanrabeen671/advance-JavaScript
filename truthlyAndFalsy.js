a93c9fd71c96c9350b2a95b393daf26113957a00
zamanrabeen671 <manbeen671@gmail.com>
2021-02-02 00:06:27 +0600

    True False

---
+++ truthlyAndFalsy.js
@@ -0,0 +1,14 @@
+//falsy
+//  0
+// undefined
+// NaN
+// null
+// ""
+// false
+const age = 5;
+if(age > 0) {
+    console.log('Condition is true');
+}
+else{
+    console.log('condition is false')
+}
