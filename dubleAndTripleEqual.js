e0b4d2d8dc375350dc74efe62d16e50883c23233
zamanrabeen671 <manbeen671@gmail.com>
2021-02-02 00:26:12 +0600

    first commit

---
+++ dubleAndTripleEqual.js
@@ -0,0 +1,15 @@
+const f = 12;
+const s = "12";
+if(f === s) {
+    console.log("Double Equal true");
+}
+else{
+    console.log("Double Equal false")
+}
+
+if(f == s) {
+    console.log("Triple Equal true");
+}
+else{
+    console.log("Triple Equal false")
+}
